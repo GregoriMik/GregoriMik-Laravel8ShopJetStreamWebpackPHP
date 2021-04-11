c5c8ef0f3a8660040ad03f63277f72512875a127
Gregori Mikina <gregorimikina@gmail.com>
2021-04-11 10:41:01 +0200

    Edit login layout, copy login.html to guest

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -480,14 +480,14 @@
 		</div>
 	</footer>
 	
-	<script src="{{ asset('/js/jquery-1.12.4.minb8ff.js?ver=1.12.4"></script>
-	<script src="{{ asset('/js/jquery-ui-1.12.4.minb8ff.js?ver=1.12.4"></script>
-	<script src="{{ asset('/js/bootstrap.min.js"></script>
-	<script src="{{ asset('/js/jquery.flexslider.js"></script>
-	<script src="{{ asset('/js/chosen.jquery.min.js"></script>
-	<script src="{{ asset('/js/owl.carousel.min.js"></script>
-	<script src="{{ asset('/js/jquery.countdown.min.js"></script>
-	<script src="{{ asset('/js/jquery.sticky.js"></script>
-	<script src="{{ asset('/js/functions.js"></script>
+	<script src="{{ asset('/js/jquery-1.12.4.minb8ff.js?ver=1.12.4') }}"></script>
+	<script src="{{ asset('/js/jquery-ui-1.12.4.minb8ff.js?ver=1.12.4') }}"></script>
+	<script src="{{ asset('/js/bootstrap.min.js') }}"></script>
+	<script src="{{ asset('/js/jquery.flexslider.js') }}"></script>
+	<script src="{{ asset('/js/chosen.jquery.min.js') }}"></script>
+	<script src="{{ asset('/js/owl.carousel.min.js') }}"></script>
+	<script src="{{ asset('/js/jquery.countdown.min.js') }}"></script>
+	<script src="{{ asset('/js/jquery.sticky.js') }}"></script>
+	<script src="{{ asset('/js/functions.js') }}"></script>
 </body>
 </html>
